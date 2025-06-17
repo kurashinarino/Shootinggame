60e513f54c2e2dc3c0050b3a6d8145f10d9e2386
Rino <kurarinorino@ymail.ne.jp>
2025-06-17 10:45:59 +0900

    10.1,10.2,10.3

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+
+public class Player extends Character{
+	public Player(double x, double y, double vx, double vy) {
+		//Characterクラスのコンストラクタ呼び出し(8章)
+		super(x, y, vx, vy);
+	}
+
+}
---
+++ src/PlayerBullet.java
@@ -0,0 +1,8 @@
+
+public class PlayerBullet extends Character{
+	public PlayerBullet(double x, double y,
+			            double vx, double vy) {
+		//Characterクラスのコンストラクタ呼び出し(8章)
+		super(x,y,vx,vy);
+	}
+}
