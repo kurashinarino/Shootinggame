07658439be18e9edb2d9e1ecce3470d6f93f0c9a
Rino <kurarinorino@ymail.ne.jp>
2025-06-17 11:56:06 +0900

    10.6,10.7,10.8

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Player extends Character implements KeyListener{
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		f.fillRect(x, y+20, 30, 10);
+		f.setColor(200,200,200);
+		f.fillRect(x+10, y, 10, 30);
+	}
+	//他のメソッド,コンストラクタは省略
 	public Player(double x, double y, double vx, double vy) {
 		//Characterクラスのコンストラクタ呼び出し(8章)
 		super(x, y, vx, vy);
@@ -15,8 +22,9 @@ public class Player extends Character implements KeyListener{
 			vx=5;
 		}
 		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
-			GameWorld.playerBullets.add(
-					new PlayerBullet(x,y,0,-10));
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+			System.out.println("弾の数="+
+					           GameWorld.playerBullets.size());
 		}
 	}
 	public void keyReleased(KeyEvent e) {
--- src/PlayerBullet.java
+++ src/PlayerBullet.java
@@ -1,5 +1,11 @@
 
 public class PlayerBullet extends Character{
+	public void draw(MyFrame f) {
+		f.setColor(0, 128, 0);
+		//f.fillRect(x, y+20, 30, 10);
+		//f.setColor(200,200,200);
+		f.fillRect(x+10, y, 10, 30);
+	}
 	public PlayerBullet(double x, double y,
 			            double vx, double vy) {
 		//Characterクラスのコンストラクタ呼び出し(8章)
